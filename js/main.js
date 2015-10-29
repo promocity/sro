85dc9f7cfea953de7fc90039d4315c331a3e7af9
unknown <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2015-10-29 13:26:18 +0500

    Готовая версия сайта на 29.10.2015

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    PopUpHide1();
+    PopUpHide2();
+    PopUpHide3();        
+});
+function PopUpShow1() {
+    $("#popup1").show();
+}
+function PopUpHide1() {
+    $("#popup1").hide();
+}
+function PopUpShow2() {
+    $("#popup2").show();
+}
+function PopUpHide2() {
+    $("#popup2").hide();
+}
+function PopUpShow3() {
+    $("#popup3").show();
+}
+function PopUpHide3() {
+    $("#popup3").hide();
+}
