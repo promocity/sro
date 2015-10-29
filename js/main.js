698831744428038f22e34869724ed9df56ce2a70
unknown <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2015-10-29 14:27:46 +0500

    Версия с рабочим валидатором

--- js/main.js
+++ js/main.js
@@ -1,7 +1,206 @@
 $(document).ready(function(){
     PopUpHide1();
     PopUpHide2();
-    PopUpHide3();        
+    PopUpHide3();
+    PopUpCompliteHide();
+
+    $('.sro-order').validate({
+   rules: {
+     email: {
+        required: true,
+        email: true
+     },
+     name: {
+      required: true,
+      minlength: 3      
+     },
+     tel: {
+      required: true,
+      minlength: 11,
+      maxlength: 11,
+      digits: true
+     },
+     password: {
+        required: true,
+        rangelength:[8,16]
+     },
+     confirm_password: {equalTo:'#password'},
+     spam: "required"
+   }, //end rules
+   messages: {
+     name: {
+       required: "Пожалуйста введите Ваше имя.",
+       minlength: "Имя не должно быть короче 3 символов."
+     },
+      email: {
+         required: "Пожалуйста введите email адрес.",
+         email: "Неверный формат email адреса."
+       },
+       tel: {
+         required: "Пожалуйста введите телефон.",
+         minlength: "Телефон должен содержать 11 символов начиная 8.",
+         digits: "Телефонный номер должен содержать только цифры."
+       },
+      password: {
+        required: 'Please type a password',
+        rangelength: 'Password must be between 8 and 16 characters long.'
+      },
+      confirm_password: {
+        equalTo: 'The two passwords do not match.'
+      }
+   }
+  }); /* close sro-order validate */
+  
+  $('.order-form').validate({
+   rules: {
+     email: {
+        required: true,
+        email: true
+     },
+     name: {
+      required: true,
+      minlength: 3      
+     },
+     tel: {
+      required: true,
+      minlength: 11,
+      maxlength: 11,
+      digits: true
+     },
+     password: {
+        required: true,
+        rangelength:[8,16]
+     },
+     confirm_password: {equalTo:'#password'},
+     spam: "required"
+   }, //end rules
+   messages: {
+     name: {
+       required: "Пожалуйста введите Ваше имя.",
+       minlength: "Имя не должно быть короче 3 символов."
+     },
+      email: {
+         required: "Пожалуйста введите email адрес.",
+         email: "Неверный формат email адреса."
+       },
+       tel: {
+         required: "Пожалуйста введите телефон +79.. ... .. ..",
+         minlength: "Телефон должен содержать 11 символов начиная 8.",
+         digits: "Телефонный номер должен содержать только цифры."
+       },
+      password: {
+        required: 'Please type a password',
+        rangelength: 'Password must be between 8 and 16 characters long.'
+      },
+      confirm_password: {
+        equalTo: 'The two passwords do not match.'
+      }
+   }
+  });  /* close order-form validate */
+  
+     $('.calc-pop-up').validate({
+   rules: {
+     email: {
+        required: true,
+        email: true
+     },
+     name: {
+      required: true,
+      minlength: 3      
+     },
+     tel: {
+      required: true,
+      minlength: 11,
+      maxlength: 11,
+      digits: true
+     },
+     password: {
+        required: true,
+        rangelength:[8,16]
+     },
+     confirm_password: {equalTo:'#password'},
+     spam: "required"
+   }, //end rules
+   messages: {
+     name: {
+       required: "Пожалуйста введите Ваше имя.",
+       minlength: "Имя не должно быть короче 3 символов."
+     },
+      email: {
+         required: "Пожалуйста введите email адрес.",
+         email: "Неверный формат email адреса."
+       },
+       tel: {
+         required: "Пожалуйста введите телефон +79.. ... .. ..",
+         minlength: "Телефон должен содержать 11 символов начиная 8.",
+         digits: "Телефонный номер должен содержать только цифры."
+       },
+      password: {
+        required: 'Please type a password',
+        rangelength: 'Password must be between 8 and 16 characters long.'
+      },
+      confirm_password: {
+        equalTo: 'The two passwords do not match.'
+      }
+   }
+  }); /* close calc pop-up validate */
+  
+   $('.custom-servise').validate({
+   rules: {
+     email: {
+        required: true,
+        email: true
+     },
+     name: {
+      required: true,
+      minlength: 3      
+     },
+     tel: {
+      required: true,
+      minlength: 11,
+      maxlength: 11,
+      digits: true
+     },
+     password: {
+        required: true,
+        rangelength:[8,16]
+     },
+     confirm_password: {equalTo:'#password'},
+     spam: "required"
+   }, //end rules
+   messages: {
+     name: {
+       required: "Пожалуйста введите Ваше имя.",
+       minlength: "Имя не должно быть короче 3 символов."
+     },
+      email: {
+         required: "Пожалуйста введите email адрес.",
+         email: "Неверный формат email адреса."
+       },
+       tel: {
+         required: "Пожалуйста введите телефон +79.. ... .. ..",
+         minlength: "Телефон должен содержать 11 символов начиная 8.",
+         digits: "Телефонный номер должен содержать только цифры."
+       },
+      password: {
+        required: 'Please type a password',
+        rangelength: 'Password must be between 8 and 16 characters long.'
+      },
+      confirm_password: {
+        equalTo: 'The two passwords do not match.'
+      }
+   }
+  }); /* close custom-service validate */
+    
+  $("body").keypress(function(e) {
+    if (e.which == 0) {
+      PopUpHide1();
+      PopUpHide2();
+      PopUpHide3();
+    return false;
+    }
+  });
+    
 });
 function PopUpShow1() {
     $("#popup1").show();
@@ -20,4 +219,13 @@ function PopUpShow3() {
 }
 function PopUpHide3() {
     $("#popup3").hide();
+}
+function PopUpCompliteShow() {
+    $("#popup-complite").show();
+}
+function PopUpCompliteHide() {
+    $("#popup-complite").hide();
+    $("#popup1").hide();    
+    $("#popup2").hide();    
+    $("#popup3").hide();    
 }
